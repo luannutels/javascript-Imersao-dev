90901eae2a798a94a2092e23bb975cbd9a3f4e82
luannutels <luannutels@gmail.com>
2022-03-19 18:52:26 -0300

    add items

---
+++ Calculadora de média/script.js
@@ -0,0 +1,14 @@
+var nome = "Luan Nutels";
+var notaDoPrimeiroBismestre = 9;
+var notaDoSegundoBismestre = 7;
+var notaDoTerceiroBismestre = 4;
+var notaDoQuartoBismestre = 2;
+
+var notaFinal =
+  notaDoPrimeiroBismestre +
+  notaDoSegundoBismestre +
+  notaDoTerceiroBismestre +
+  notaDoQuartoBismestre / 4;
+
+console.log("Bem Vindo" + nome);
+console.log(notaFinal);
